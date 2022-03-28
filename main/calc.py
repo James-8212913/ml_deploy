ff46349c6067d3b1ae15bc8aaff2e7b5e75057a5
James M <james.w.murray@student.uts.edu.au>
2022-03-28 17:08:28 +1100

    updated deployment framework

---
+++ main/calc.py
@@ -0,0 +1,10 @@
+# Take two numbers as user input, add them and find the difference between them
+
+## import the modules from the src folder
+
+from src import add 
+
+print("This take two numbers, add them then find the difference between them")
+
+x = int(input("Enter the first number: "))
+y = int(input("Enter the second number: "))
